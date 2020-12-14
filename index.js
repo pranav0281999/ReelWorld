712d6d871f914bf3adff1b8dcd0a0c8b2c404953
pranav0281999 <pranav0281999@gmail.com>
2020-12-14 15:09:59 +0530

    Refactors code

--- index.js
+++ index.js
@@ -3,23 +3,60 @@ import {World} from "./world.js";
 const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', init);
 
-function init() {
-    //remove the overlay as no longer needed
-    document.getElementById("overlay").remove();
+let world = null;
 
+function init() {
+    callConnect();
+
+    const shareScreenButton = document.getElementById("shareScreen");
+    shareScreenButton.addEventListener('click', shareScreen);
+}
+
+function callConnect() {
+    //remove the overlay as no longer needed, show call options
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("conferenceOptions").style.display = "flex";
+
+    //get local media stream, create the world and pass on stream
     let localMediaStream = navigator.mediaDevices.getUserMedia({
-        audio: true,
+        audio: false,
         video: {
             aspectRatio: 1
         }
     }).then((mediaStream) => {
-        let video = document.createElement( 'video' );
+        let video = document.createElement('video');
         video.srcObject = mediaStream;
         video.id = "local_video";
         video.play().then(() => {
             console.log("Local video playing");
         });
 
-        new World(video).init();
+        world = new World(video);
+        world.init();
+    });
+}
+
+function callDisconnect() {
+    //remove the call options as no longer needed, show overlay
+    document.getElementById("conferenceOptions").style.display = "none";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function shareScreen() {
+    let screenStream = navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true
+    }).then((mediaStream) => {
+        let video = document.createElement('video');
+        video.srcObject = mediaStream;
+        video.id = "local_video";
+        video.play().then(() => {
+            console.log("Local screen playing");
+        });
+
+        world.localVideoStream = mediaStream;
+    }).catch(err => {
+        console.error("Error:" + err);
+        return null;
     });
 }
--- world.js
+++ world.js
@@ -5,37 +5,42 @@ import {CSS2DRenderer, CSS2DObject} from "./lib/CSS2DRenderer.js";
 class World {
     constructor(videoStream) {
         this.localVideoStream = videoStream;
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera();
+        this.renderer = new THREE.WebGLRenderer();
+        this.controls = new PlayerControls(this.camera, new THREE.Object3D());
+        this.labelRenderer = new CSS2DRenderer();
     }
 
-    init() {
+    init = () => {
         const canvas = document.getElementById("c");
-        let renderer = new THREE.WebGLRenderer({canvas});
+        this.renderer = new THREE.WebGLRenderer({canvas});
 
-        let scene = new THREE.Scene();
-        scene.background = 0x000000;
+        this.scene = new THREE.Scene();
+        this.scene.background = 0x000000;
 
         //ambient light to reduce computations and keep everything visible
         const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
-        scene.add(ambientLight);
+        this.scene.add(ambientLight);
 
         //setting this so that center is known
         const axesHelper = new THREE.AxesHelper(5);
         axesHelper.position.y = 1;
-        scene.add(axesHelper);
+        this.scene.add(axesHelper);
 
         //setting the stage
         let gridHelper = new THREE.GridHelper(200, 20);
-        scene.add(gridHelper);
+        this.scene.add(gridHelper);
 
         //camera to follow user around
         const fov = 90;
         const aspect = window.innerWidth / window.innerHeight;
         const far = 283;
         const near = 1;
-        let camera = new THREE.PerspectiveCamera(fov, aspect, far, near);
-        camera.position.y = 10;
-        camera.position.z = 15;
-        camera.lookAt(0, 0, 0);
+        this.camera = new THREE.PerspectiveCamera(fov, aspect, far, near);
+        this.camera.position.y = 10;
+        this.camera.position.z = 15;
+        this.camera.lookAt(0, 0, 0);
 
         //setting up user
         const userLowerBodyGeo = new THREE.BoxGeometry(5, 5, 5);
@@ -53,69 +58,69 @@ class World {
         userBody.add(userUpperBodyMesh);
         userBody.add(userLowerBodyMesh);
 
-        let userLabelDiv = document.createElement( 'div' );
+        let userLabelDiv = document.createElement('div');
         userLabelDiv.className = 'label';
         userLabelDiv.textContent = 'You';
         userLabelDiv.style.marginTop = '-2em';
         userLabelDiv.style.color = "white";
         userLabelDiv.style.fontSize = "2em";
-        const userLabel = new CSS2DObject( userLabelDiv );
-        userLabel.position.set( 0, 11, 0 );
-        userBody.add( userLabel );
+        const userLabel = new CSS2DObject(userLabelDiv);
+        userLabel.position.set(0, 11, 0);
+        userBody.add(userLabel);
 
         let user = new THREE.Group();
         user.add(userBody);
-        user.add(camera);
+        user.add(this.camera);
 
-        scene.add(user);
+        this.scene.add(user);
 
         //setting up movement controls
         //I have not used the main camera here as I didn't like the camera movement provided by the library
-        let controls = new PlayerControls(camera, user);
-        controls.moveSpeed = 2;
-        controls.turnSpeed = 0.1;
-        controls.maxDistanceFromCenter = 100;
+        this.controls = new PlayerControls(this.camera, user);
+        this.controls.moveSpeed = 2;
+        this.controls.turnSpeed = 0.1;
+        this.controls.maxDistanceFromCenter = 100;
 
         //to show labels
-        let labelRenderer = new CSS2DRenderer();
-        labelRenderer.setSize( window.innerWidth, window.innerHeight );
-        labelRenderer.domElement.style.position = 'absolute';
-        labelRenderer.domElement.style.top = '0px';
-        document.body.appendChild( labelRenderer.domElement );
+        this.labelRenderer = new CSS2DRenderer();
+        this.labelRenderer.setSize(window.innerWidth, window.innerHeight);
+        this.labelRenderer.domElement.style.position = 'absolute';
+        this.labelRenderer.domElement.style.top = '0px';
+        document.body.appendChild(this.labelRenderer.domElement);
 
-        function resizeRendererToDisplaySize(renderer) {
-            const canvas = renderer.domElement;
-            const pixelRatio = window.devicePixelRatio;
-            const width = canvas.clientWidth * pixelRatio | 0;
-            const height = canvas.clientHeight * pixelRatio | 0;
-            const needResize = canvas.width !== width || canvas.height !== height;
+        requestAnimationFrame(this.render);
+    }
 
-            if (needResize) {
-                renderer.setSize(width, height, false);
-                labelRenderer.setSize( width, height );
-            }
+    resizeRendererToDisplaySize = () => {
+        const canvas = this.renderer.domElement;
+        const pixelRatio = window.devicePixelRatio;
+        const width = canvas.clientWidth * pixelRatio | 0;
+        const height = canvas.clientHeight * pixelRatio | 0;
+        const needResize = canvas.width !== width || canvas.height !== height;
 
-            return needResize;
+        if (needResize) {
+            this.renderer.setSize(width, height, false);
+            this.labelRenderer.setSize(width, height);
         }
 
-        function render(time) {
-            if (resizeRendererToDisplaySize(renderer)) {
-                const canvas = renderer.domElement;
-                camera.aspect = canvas.clientWidth / canvas.clientHeight;
-                camera.updateProjectionMatrix();
-            }
+        return needResize;
+    }
 
-            controls.update();
-
-            renderer.render(scene, camera);
-            labelRenderer.render( scene, camera );
-
-            setTimeout(function () {
-                requestAnimationFrame(render);
-            }, 1000 / 30);
+    render = (time) => {
+        if (this.resizeRendererToDisplaySize()) {
+            const canvas = this.renderer.domElement;
+            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            this.camera.updateProjectionMatrix();
         }
 
-        requestAnimationFrame(render);
+        this.controls.update();
+
+        this.renderer.render(this.scene, this.camera);
+        this.labelRenderer.render(this.scene, this.camera);
+
+        setTimeout(() => {
+            requestAnimationFrame(this.render);
+        }, 1000 / 30);
     }
 }
 
